298ddac8c59ecbc9416e44741c1ec335b8d7e6f8
Muhammad Mehroz <chm.Rose337@gmail.com>
2024-08-10 03:09:37 +0500

    deleted junk files

--- Lecture_2.py
+++
@@ -1,9 +0,0 @@
-import  pandas as pd
-import matplotlib.pyplot as plt
-
-#auto = pd.read_csv("Automobile_data.csv")
-accidents = pd.read_csv("US_Accidents_Dec21_updated.csv")
-
-print(accidents.columns)
-#
-# print((accidents['Wind_Speed(mph)'].head()))
